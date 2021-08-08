e5c325e7244ed1f069f0e8179af24b824eb28a15
MingTaoChen <mail@mittacy.com>
2021-08-08 12:38:19 +0800

    service层bug

--- app/service/user.go
+++ app/service/user.go
@@ -9,18 +9,18 @@ import (
 // 实现api层中的service接口
 
 type User struct {
-	userService IUserService
-	logger      *logger.CustomLogger
+	userData IUserData
+	logger   *logger.CustomLogger
 }
 
-func NewUser(userService IUserService, logger *logger.CustomLogger) api.IUserService {
+func NewUser(userData IUserData, logger *logger.CustomLogger) api.IUserService {
 	return &User{
-		userService: userService,
-		logger:      logger,
+		userData: userData,
+		logger:   logger,
 	}
 }
 
-type IUserService interface {
+type IUserData interface {
 	Insert(user *model.User) error
 	Delete(id int64) error
 	UpdateById(user model.User, updateFields []string) error
@@ -30,7 +30,7 @@ type IUserService interface {
 }
 
 func (ctl *User) Create(user model.User) (int64, error) {
-	if err := ctl.userService.Insert(&user); err != nil {
+	if err := ctl.userData.Insert(&user); err != nil {
 		return 0, nil
 	}
 
@@ -38,13 +38,13 @@ func (ctl *User) Create(user model.User) (int64, error) {
 }
 
 func (ctl *User) Delete(id int64) error {
-	return ctl.userService.Delete(id)
+	return ctl.userData.Delete(id)
 }
 
 func (ctl *User) UpdateInfo(user model.User) error {
 	fields := []string{"name"}
 
-	return ctl.userService.UpdateById(user, fields)
+	return ctl.userData.UpdateById(user, fields)
 }
 
 func (ctl *User) UpdatePassword(id int64, password string) error {
@@ -54,22 +54,22 @@ func (ctl *User) UpdatePassword(id int64, password string) error {
 		Password: password, // 加密等操作
 	}
 
-	return ctl.userService.UpdateById(user, fields)
+	return ctl.userData.UpdateById(user, fields)
 }
 
 func (ctl *User) Get(id int64) (*model.User, error) {
-	return ctl.userService.Select(id)
+	return ctl.userData.Select(id)
 }
 
 func (ctl *User) List(page, pageSize int) ([]model.User, int64, error) {
 	fields := []string{"id", "name", "created_at", "updated_at"}
 
-	list, err := ctl.userService.List(fields, page, pageSize)
+	list, err := ctl.userData.List(fields, page, pageSize)
 	if err != nil {
 		return nil, 0, err
 	}
 
-	totalSize, err := ctl.userService.SelectSum()
+	totalSize, err := ctl.userData.SelectSum()
 	if err != nil {
 		return nil, 0, err
 	}
