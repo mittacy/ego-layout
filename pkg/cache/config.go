d4557fb0ed42275006661904090a8bea0e6045ba
MingTaoChen <mail@mittacy.com>
2021-12-15 14:25:02 +0800

    相同的redis连接使用同一个缓冲池

--- pkg/cache/config.go
+++ pkg/cache/config.go
@@ -1,11 +1,17 @@
 package cache
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+	"time"
+)
 
 type Conf struct {
-	Host     string
-	Password string
-	Port     int
+	Host        string
+	Password    string
+	Port        int
+	PoolSize    int
+	MinIdleConn int
+	IdleTimeout time.Duration
 }
 
 func GetConfig(name string) (Conf, bool) {
@@ -14,9 +20,12 @@ func GetConfig(name string) (Conf, bool) {
 	switch name {
 	case "localhost":
 		conf = Conf{
-			Host:     viper.GetString("REDIS_LOCALHOST_RW_HOST"),
-			Password: viper.GetString("REDIS_LOCALHOST_RW_PASSWORD"),
-			Port:     viper.GetInt("REDIS_LOCALHOST_RW_PORT"),
+			Host:        viper.GetString("REDIS_LOCALHOST_RW_HOST"),
+			Password:    viper.GetString("REDIS_LOCALHOST_RW_PASSWORD"),
+			Port:        viper.GetInt("REDIS_LOCALHOST_RW_PORT"),
+			PoolSize:    viper.GetInt("REDIS_LOCALHOST_POOL_SIZE"),
+			MinIdleConn: viper.GetInt("REDIS_LOCALHOST_MIN_IDLE_CONN"),
+			IdleTimeout: viper.GetDuration("REDIS_LOCALHOST_IDLE_TIMEOUT"),
 		}
 	default:
 		return Conf{}, false
--- pkg/cache/redis.go
+++ pkg/cache/redis.go
@@ -5,15 +5,31 @@ import (
 	"fmt"
 	"github.com/go-redis/redis/v8"
 	"log"
+	"time"
 )
+
+var (
+	cachePool map[string]*redis.Client
+)
+
+func init() {
+	cachePool = make(map[string]*redis.Client, 0)
+}
 
 // NewClientByName 直接通过配置名字获取新客户端
 // @param name 配置名
 // @param db 使用哪个数据库
 // @return *redis.Client
 func NewClientByName(name string, db int) *redis.Client {
+	cacheName := cachePoolName(name, db)
+	if c, ok := cachePool[cacheName]; ok {
+		return c
+	}
+
 	if conf, ok := GetConfig(name); ok {
-		return NewClient(conf, db)
+		client := NewClient(conf, db)
+		cachePool[cacheName] = client
+		return client
 	}
 
 	log.Panicf("%s 配置不存在, 请在 pkg/cache/config.go GetConfig() 中配置", name)
@@ -25,15 +41,30 @@ func NewClientByName(name string, db int) *redis.Client {
 // @param db 使用哪个数据库
 // @return *redis.Client
 func NewClient(conf Conf, db int) *redis.Client {
-	rdb := redis.NewClient(&redis.Options{
+	options := &redis.Options{
 		Addr:     fmt.Sprintf("%s:%d", conf.Host, conf.Port),
 		Password: conf.Password,
 		DB:       db,
-	})
+	}
 
+	if conf.PoolSize > 0 { // 最大连接数
+		options.PoolSize = conf.PoolSize
+	}
+	if conf.MinIdleConn > 0 { // 最小空闲连接数
+		options.MinIdleConns = conf.MinIdleConn
+	}
+	if conf.IdleTimeout > 0 { // 空闲时间(秒)
+		options.IdleTimeout = conf.IdleTimeout * time.Second
+	}
+
+	rdb := redis.NewClient(options)
 	if err := rdb.Ping(context.Background()).Err(); err != nil {
 		log.Panicf("连接数据库失败, 检查配置, err: %s, conf: %+v", err, conf)
 	}
 
 	return rdb
+}
+
+func cachePoolName(name string, db int) string {
+	return fmt.Sprintf("%s:%d", name, db)
 }
