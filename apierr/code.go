6381ceb3d09bd762c08c7541bcb3b07eb84c1c3f
MingTaoChen <mail@mittacy.com>
2022-02-02 16:21:14 +0800

    feat: 错误码重定义

--- apierr/code.go
+++ apierr/code.go
@@ -6,7 +6,10 @@ import (
 
 // 业务错误定义格式: 大模块:中间模块:业务模块
 var (
-	//用户相关 code: 1000XX
-	LoginExpire = &apierr.BizErr{Code: 100001, Msg: "登录信息过期"}
-	UserNoExist = &apierr.BizErr{Code: 100002, Msg: "用户不存在"}
+	// 共用 code: 1000XX
+	ResourceNoExist = &apierr.BizErr{Code: 100001, Msg: "资源不存在"}
+
+	//用户相关 code: 2000XX
+	LoginExpire = &apierr.BizErr{Code: 200001, Msg: "登录信息过期"}
+	UserNoExist = &apierr.BizErr{Code: 200002, Msg: "用户不存在"}
 )
